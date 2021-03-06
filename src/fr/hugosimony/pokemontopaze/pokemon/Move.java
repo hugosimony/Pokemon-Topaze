b82b0254f5b13a987a24538782d75e603a84dcd0
CS-Sauvaj <hugosimonyjungo@aol.fr>
2021-03-06 17:20:35 +0000

    Finished attack moves

--- src/fr/hugosimony/pokemontopaze/pokemon/Move.java
+++ src/fr/hugosimony/pokemontopaze/pokemon/Move.java
@@ -2,4 +2,24 @@ package fr.hugosimony.pokemontopaze.pokemon;
 
 public class Move {
 
+	public String name;
+	public String description;
+	public Type type;
+	public Type category;
+	public int power;
+	public int precision;
+	public int pp;
+	
+	public Move(Moves move) {
+		
+		String[] data = Moves.getMoveData(move);
+		name = data[0];
+		description = data[2];
+		type = Type.getTypeFromString(data[1]);
+		category = Type.getCategoryFromString(data[6]);
+		power = Integer.parseInt(data[3]);
+		precision = Integer.parseInt(data[4]);
+		pp = Integer.parseInt(data[5]);
+	}
+	
 }
--- src/fr/hugosimony/pokemontopaze/pokemon/Type.java
+++ src/fr/hugosimony/pokemontopaze/pokemon/Type.java
@@ -21,10 +21,60 @@ public enum Type {
 	TENEBRES,
 	VOL,
 	
-	AUCUN,
-	
 	PHYSIQUE,
 	SPECIALE,
-	STATUT
+	STATUT,
+	
+	AUCUN;
+	
+	public static Type getTypeFromString(String type) {
+		if(type.equalsIgnoreCase("ACIER"))
+			return ACIER;
+		if(type.equalsIgnoreCase("COMBAT"))
+			return COMBAT;
+		if(type.equalsIgnoreCase("DRAGON"))
+			return DRAGON;
+		if(type.equalsIgnoreCase("EAU"))
+			return EAU;
+		if(type.equalsIgnoreCase("ELECTRIK"))
+			return ELECTRIK;
+		if(type.equalsIgnoreCase("FEE"))
+			return FEE;
+		if(type.equalsIgnoreCase("FEU"))
+			return FEU;
+		if(type.equalsIgnoreCase("GLACE"))
+			return GLACE;
+		if(type.equalsIgnoreCase("INSECTE"))
+			return INSECTE;
+		if(type.equalsIgnoreCase("NORMAL"))
+			return NORMAL;
+		if(type.equalsIgnoreCase("PLANTE"))
+			return PLANTE;
+		if(type.equalsIgnoreCase("POISON"))
+			return POISON;
+		if(type.equalsIgnoreCase("PSY"))
+			return PSY;
+		if(type.equalsIgnoreCase("ROCHE"))
+			return ROCHE;
+		if(type.equalsIgnoreCase("SOL"))
+			return SOL;
+		if(type.equalsIgnoreCase("SPECTRE"))
+			return SPECTRE;
+		if(type.equalsIgnoreCase("TENEBRES"))
+			return TENEBRES;
+		if(type.equalsIgnoreCase("VOL"))
+			return VOL;
+		return AUCUN;
+	}
+	
+	public static Type getCategoryFromString(String category) {
+		if(category.equalsIgnoreCase("PHYSIQUE"))
+			return PHYSIQUE;
+		if(category.equalsIgnoreCase("SPECIALE"))
+			return SPECIALE;
+		if(category.equalsIgnoreCase("STATUT"))
+			return STATUT;
+		return AUCUN;
+	}
 	
 }
